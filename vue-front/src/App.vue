d170532f505a994f079e8d64dff10339701e0834
Suylo <spoownz@yahoo.com>
2023-10-30 16:13:58 +0100

    Remove unused Vue components and assets
    Removed a series of Vue components and assets files that are not necessary for the current project implementation. These removals include Vue components such as 'AboutView.vue', 'HelloWorld.vue', icons, and assets like 'base.css', 'main.css', and 'logo.svg'. This clean-up is aimed at reducing the project's complexity and ensuring that the codebase only contains elements that contribute to the functionality of the application.

--- vue-front/src/App.vue
+++ vue-front/src/App.vue
@@ -1,85 +1,49 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+    <header class='header'>
+      <nav class='nav'>
+        <RouterLink to="/" class='nav__link'>Accueil</RouterLink>
+        <RouterLink to="/galerie" class='nav__link'>Galerie</RouterLink>
+        <RouterLink to="/" class='nav__link'>Artistes</RouterLink>
       </nav>
-    </div>
-  </header>
+    </header>
 
-  <RouterView />
+    <main>
+        <RouterView />
+    </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<style scoped lang='scss'>
+.header{
+	background-color: $white;
+	border-bottom: 3px solid $black;
+	justify-content: center;
+	display: flex;
+	align-items: center;
+	padding: 4rem 2rem;
+	.nav{
+		&__link{
+			color: $black;
+			text-transform: uppercase;
+			text-decoration: none;
+			font-weight: 500;
+			font-family: $jakarta;
+			font-size: 3rem;
+			display: inline-block;
+			margin: 0 3rem;
+			transition: all .2s ease-in-out;
+		}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+		&:hover .nav__link:not(:hover){
+			opacity: 0.5;
+		}
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+		&__link:hover{
+			color: $blue;
+		}
+	}
 }
 </style>
--- vue-front/src/views/HomeView.vue
+++ vue-front/src/views/HomeView.vue
@@ -1,9 +1,115 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+	<section class='home'>
+		<div class='home__banner'>
+			<div class='home__banner__text'>
+				<h1>Bienvenue sur le site de la galerie d'art</h1>
+				<p>Cliquez ci-dessous pour découvrir toutes les oeuvres</p>
+				<RouterLink to='/' class='btn'>Découvrir la galerie</RouterLink>
+			</div>
+			<div class='home__banner__img'>
+				<img src='/arts/img/oeuvre1.jpeg' alt='home' />
+				<img src='/arts/img/oeuvre2.jpeg' alt='home' class='home__banner__img__absolute'>
+				<img src='/arts/img/oeuvre3.jpeg' alt='home' class='home__banner__img__absolute2'>
+			</div>
+		</div>
+
+
+		<div class='home__text-img'>
+			<div class='home__text-img__text'>
+				<h2>La galerie d'art</h2>
+				<p>Lorem ipsum dolor sit amet consectetur ad</p>
+			</div>
+			<div class='home__text-img__img'>
+				<img src='/arts/img/oeuvre1.jpeg' alt='home' />
+			</div>
+		</div>
+	</section>
 </template>
+
+<style lang='scss' scoped>
+.home{
+	h1{
+		font-family: $jakarta;
+		font-size: 4rem;
+		font-weight: 600;
+	}
+
+	h2{
+		font-family: $jakarta;
+		font-size: 3rem;
+		font-weight: 600;
+	}
+
+	p{
+		font-family: $opensans;
+		font-size: 1.6rem;
+		font-weight: 400;
+		margin: 2rem 0;
+	}
+
+
+	&__banner{
+		background-color: $blue;
+		padding: 5rem 2rem;
+		color: $white;
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		place-items: center;
+		border-bottom: 3px solid $black;
+
+
+		&__text{
+			animation: fadeLeft .3s ease-in-out;
+		}
+
+		&__img{
+			position: relative;
+			z-index: 2;
+			animation: fadeRight .2s ease-in-out;
+			img{
+				max-width: 512px;
+				width: 100%;
+				height: 100%;
+				object-fit: cover;
+			}
+			&__absolute{
+				position: absolute;
+				top: 2rem;
+				left: 6rem;
+				animation: fadeRight .2s ease-in-out;
+				z-index: 3;
+			}
+			&__absolute2{
+				position: absolute;
+				bottom: 2rem;
+				right: 6rem;
+				z-index: -1;
+				animation: fadeRight .2s ease-in-out;
+			}
+		}
+	}
+
+	&__text-img{
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+
+		&__text{
+			padding: 4rem;
+			box-sizing: border-box;
+		}
+
+		&__img{
+			border-left: 3px solid $black;
+			img{
+
+				width: 100%;
+				height: 100%;
+				object-fit: cover;
+			}
+		}
+	}
+}
+</style>
